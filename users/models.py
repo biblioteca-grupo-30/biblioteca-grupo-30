bf510efe266f14160f15d9f2dbdfd237a42aa91a
Olivio Freitas <ojfneto92@gmail.com>
2023-05-03 10:23:25 -0300

    feat: user model created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class StatusOptions(models.TextChoices):
+    ACTIVE = "Active"
+    INACTIVE = "Inactive"
+    BLOCKED = "Blocked"
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=127, unique=True)
+    password = models.CharField(max_length=127)
+    status = models.CharField(
+        max_length=20,
+        choices=StatusOptions.choices,
+        default=StatusOptions.ACTIVE
+    )
+    is_superuser = models.BooleanField(default=False)
