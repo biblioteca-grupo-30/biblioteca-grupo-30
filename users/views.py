6f15e48470e98e616b4d2e78f9fb2a817a46564b
Olivio Freitas <ojfneto92@gmail.com>
2023-05-03 10:45:56 -0300

    feat: created user views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from .models import User
+from .serializers import UserSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView
+)
 
-# Create your views here.
+
+class UserView(ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetailView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    lookup_url_kwarg = "user_id"
