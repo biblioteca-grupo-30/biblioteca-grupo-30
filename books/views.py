1a6d88163188cb22bfb8b4c66ff8ca1b5e89fc88
Camila Gonçalves Domingos Tavares <105559819+eucamila22@users.noreply.github.com>
2023-05-03 07:51:56 -0300

    fix/list books  by id

--- books/views.py
+++ books/views.py
@@ -1,13 +1,13 @@
-from rest_framework.generics import ListCreateAPIView
-# from rest_framework import generics
 from .models import Books
 from .serializers import BookSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 
 class BookView(ListCreateAPIView):
     queryset = Books.objects.all()
     serializer_class = BookSerializer
 
-# class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Books.objects.all()
-#     serializer_class = BookSerializer
+class BookRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = "id"
