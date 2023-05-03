3432c8f36ac6a3f84015a2eb52a935cb89acb143
Marcos Melo <69183490+marcosmelo0@users.noreply.github.com>
2023-05-03 09:29:04 -0300

    fix: page size 5

--- exemplaries/urls.py
+++ exemplaries/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ExemplaryView, ExemplaryDetailView
 
 urlpatterns = [
-    path("exemplaries/<int:pk>", ExemplaryView.as_view()),
+    path("exemplaries/<int:pk>/", ExemplaryView.as_view()),
     path("exemplaries-detail/<int:pk>/", ExemplaryDetailView.as_view()),
 ]
