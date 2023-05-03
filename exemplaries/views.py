1e23b4d1f195ce5ac855118cb5d45dd120e44d37
Marcos Melo <69183490+marcosmelo0@users.noreply.github.com>
2023-05-03 11:30:19 -0300

    fix: urls of relations

--- exemplaries/views.py
+++ exemplaries/views.py
@@ -1,22 +1,21 @@
 from .models import Exemplary
 from .serializers import ExemplarySerializer
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.pagination import PageNumberPagination
 from django.shortcuts import get_object_or_404
 from books.models import Books
 
 
-class ExemplaryView(ListCreateAPIView, PageNumberPagination):
+class ExemplaryView(CreateAPIView):
     queryset = Exemplary.objects.all()
     serializer_class = ExemplarySerializer
 
     def perform_create(self, serializer):
-        book = get_object_or_404(Books, pk=self.kwargs.get("pk"))
 
+        book = get_object_or_404(Books, pk=self.kwargs.get("pk"))
         serializer.save(book=book)
 
 
-class ExemplaryDetailView(RetrieveUpdateDestroyAPIView):
+class ExamplaryRetriveUpdate(RetrieveUpdateDestroyAPIView, PageNumberPagination):
     queryset = Exemplary.objects.all()
     serializer_class = ExemplarySerializer
-
