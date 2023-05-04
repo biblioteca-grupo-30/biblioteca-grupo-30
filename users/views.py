eaac986d7322e8f0a8f6564f69a7e142a1ccfade
Olivio Freitas <ojfneto92@gmail.com>
2023-05-04 13:53:10 -0300

    feat: altered user patch

--- users/views.py
+++ users/views.py
@@ -1,11 +1,13 @@
 from .models import User
 from .serializers import UserSerializer
-# from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.generics import (
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView
 )
 from rest_framework.pagination import PageNumberPagination
+from django.shortcuts import get_object_or_404
+from rest_framework.views import Request, Response, status
 
 
 class UserView(ListCreateAPIView, PageNumberPagination):
@@ -21,4 +23,22 @@ class UserDetailView(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    lookup_url_kwarg = "user_id"
+    lookup_url_kwarg = "pk"
+
+    def get(self, request: Request, user_id: int) -> Response:
+        user = get_object_or_404(User, pk=user_id)
+        serializer = UserSerializer(user)
+        self.check_object_permissions(request, user)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def patch(self, request: Request, user_id: int) -> Response:
+        user = get_object_or_404(User, pk=user_id)
+
+        self.check_object_permissions(request, user)
+
+        serializer = UserSerializer(user, request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_200_OK)
