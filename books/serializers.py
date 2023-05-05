4186a18ae86270216f661a1a0b620c671ed3d7c4
Camila Gonçalves Domingos Tavares <105559819+eucamila22@users.noreply.github.com>
2023-05-05 14:05:16 -0300

    fix/keeping style convention

--- books/serializers.py
+++ books/serializers.py
@@ -2,12 +2,15 @@ from rest_framework import serializers
 from exemplaries.models import Exemplary
 from .models import Book
 
+
 class BookSerializer(serializers.ModelSerializer):
     is_available = serializers.SerializerMethodField()
+
 
     class Meta:
         model = Book
         fields = ["id", "title", "author", "pages", "synopsis", "category", "is_available"]
+
 
     def get_is_available(self, obj):
         available_count = Exemplary.objects.filter(book=obj, quantity__gt=0).count()
