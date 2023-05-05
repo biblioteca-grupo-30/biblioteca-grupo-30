6ff8b8768d6b8d3a8b7553bf588320664aaa44ad
gabriel <gabrielcoloradogvda@gmail.com>
2023-05-05 11:07:29 -0300

    feat: make returning view

--- loans/views.py
+++ loans/views.py
@@ -1,7 +1,8 @@
 from datetime import timedelta
 from django.forms import ValidationError
 from django.utils import timezone
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -21,26 +22,28 @@ class LoanListCreateAPIView(generics.ListCreateAPIView):
         exemplary = get_object_or_404(Exemplary, pk=serializer.
                                       validated_data["exemplary"].id)
         user = self.request.user
+        book = exemplary.book
 
-        if Loan.objects.filter(user=user,
-                               exemplary__book=exemplary.book).exists():
-            raise ValidationError("Usuário já tem um exemplar \
-emprestado para este livro")
+        if Loan.objects.filter(user=user, exemplary__book=book,
+                               returned_date__isnull=True).exists():
+            raise ValidationError(
+                "Usuário já tem um exemplar emprestado para este livro."
+                )
 
-        duration = serializer.validated_data.get("duration", exemplary
-                                                 .default_loan_duration)
+        if Loan.objects.filter(user=user, exemplary__book=book,
+                               returned_date__isnull=False).exists():
 
-        return_date = timezone.now() + timedelta(days=duration)
+            duration = serializer.validated_data.get(
+                "duration", exemplary.default_loan_duration)
 
-        if return_date.weekday() >= 5:
-            return_date += timedelta(days=2)
+            return_date = timezone.now() + timedelta(days=duration)
+            if return_date.weekday() >= 5:
+                return_date += timedelta(days=2)
+            loan = serializer.save(user=user, return_date=return_date)
+            exemplary.quantity -= 1
+            exemplary.save()
 
-        loan = serializer.save(user=user, return_date=return_date)
-
-        exemplary.quantity -= 1
-        exemplary.save()
-
-        return loan
+            return loan
 
 
 class LoanRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
@@ -48,3 +51,27 @@ class LoanRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = LoanSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class LoanReturnAPIView(generics.UpdateAPIView):
+    serializer_class = LoanSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Loan.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        loan = self.get_object()
+
+        if loan.returned_date is not None:
+            return Response({"detail": "Este exemplar já foi devolvido."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        loan.returned_date = timezone.now()
+        loan.save()
+
+        exemplary = loan.exemplary
+        exemplary.quantity += 1
+        exemplary.save()
+
+        serializer = self.get_serializer(loan)
+        return Response(serializer.data)
