56113e5dd2096867770a989d439c78b2c0bb75d6
gabriel <gabrielcoloradogvda@gmail.com>
2023-05-05 14:48:57 -0300

    feat: make returning views

--- users/serializers.py
+++ users/serializers.py
@@ -4,13 +4,18 @@ from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data: dict) -> User:
-        return User.objects.create_superuser(**validated_data)
+        is_superuser = validated_data.pop("is_superuser")
+
+        if is_superuser:
+            return User.objects.create_superuser(**validated_data)
+
+        return User.objects.create_user(**validated_data)
 
     def update(self, instance: User, validated_data: dict) -> User:
         for key, value in validated_data.items():
             setattr(instance, key, value)
-
-        instance.set_password(validated_data["password"])
+            if key == "password":
+                instance.set_password(value)
 
         instance.save()
 
@@ -24,20 +29,14 @@ class UserSerializer(serializers.ModelSerializer):
             "email",
             "password",
             "is_blocked",
-            "is_blocked",
             "is_superuser",
-            "books_followed",
-            "blocked_until"
             "books_followed",
             "blocked_until"
         ]
         extra_kwargs = {
-
             "password": {"write_only": True},
             "books_followed": {"read_only": True},
             "is_blocked": {"default": False},
             "is_superuser": {"default": False},
-            "books_followed": {"read_only": True},
-            "is_blocked": {"default": False},
-            "is_superuser": {"default": False},
+
         }
