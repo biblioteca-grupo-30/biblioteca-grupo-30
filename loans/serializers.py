2a16377588085f648af02eb29be5cb87b171f98f
Olivio Freitas <ojfneto92@gmail.com>
2023-05-08 15:01:18 -0300

    feat: bug loan commit. God save us!

--- loans/serializers.py
+++ loans/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Loan
 from followers.utils import send_mail_on_change
 from books.models import Book
+from exemplaries.models import Exemplary
 from django.shortcuts import get_object_or_404
 
 
@@ -17,15 +18,27 @@ class LoanSerializer(serializers.ModelSerializer):
                         "user": {"read_only": True},
                         "id": {"read_only": True}}
 
-    def create(self, validated_data):
-        book = get_object_or_404(Book, pk=book.id)
+    # def create(self, validated_data):
+    #     exemplary = get_object_or_404(
+    #         Exemplary,
+    #         pk=validated_data["exemplary"].id
+    #     )
+    #     book = get_object_or_404(
+    #         Book,
+    #         pk=exemplary.id
+    #     )
 
-        instance_updated = super().update(book, validated_data)
-        quantity = instance_updated.quantity
-        if quantity == 0:
-            send_mail_on_change(instance_updated, book.title, "indisponível")
+    #     instance_updated = super().update(exemplary, validated_data)
+    #     quantity = instance_updated.quantity
 
-        if validated_data["quantity"] > 0:
-            send_mail_on_change(instance_updated, book.title, "disponível")
+    #     import ipdb
 
-        return instance_updated
+    #     if quantity == 0:
+    #         send_mail_on_change(instance_updated, book.title, "indisponível")
+
+    #     if quantity > 0:
+    #         send_mail_on_change(instance_updated, book.title, "disponível")
+
+    #     ipdb.set_trace()
+
+    #     return instance_updated
