604cffbb0a6013d14d06c3a507647509324a2d6a
Olivio Freitas <ojfneto92@gmail.com>
2023-05-08 14:36:34 -0300

    feat: mailing loan

--- loans/serializers.py
+++ loans/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from .models import Loan
+from followers.utils import send_mail_on_change
+from books.models import Book
+from django.shortcuts import get_object_or_404
 
 
 class LoanSerializer(serializers.ModelSerializer):
@@ -11,3 +14,16 @@ class LoanSerializer(serializers.ModelSerializer):
         extra_kwargs = {"return_date": {"read_only": True},
                         "loan_date": {"read_only": True},
                         "user": {"read_only": True}}
+
+    def create(self, validated_data):
+        book = get_object_or_404(Book, pk=book.id)
+
+        instance_updated = super().update(book, validated_data)
+        quantity = instance_updated.quantity
+        if quantity == 0:
+            send_mail_on_change(instance_updated, book.title, "indisponível")
+
+        if validated_data["quantity"] > 0:
+            send_mail_on_change(instance_updated, book.title, "disponível")
+
+        return instance_updated
