9a16fc1461e5fbb86ab10a177c88eeb71359af71
Marcos Melo <69183490+marcosmelo0@users.noreply.github.com>
2023-05-09 10:42:10 -0300

    feat: add student and employee loan listing

--- loans/urls.py
+++ loans/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from .views import (
     LoanListCreateAPIView,
     LoanRetrieveUpdateDestroyAPIView,
-    LoanReturnAPIView)
+    LoanReturnAPIView,
+    ListLoanOwner)
 
 urlpatterns = [
     path("loan/", LoanListCreateAPIView.as_view()),
     path("loans/<int:pk>/", LoanRetrieveUpdateDestroyAPIView.as_view()),
     path("loans/returning/<int:pk>/", LoanReturnAPIView.as_view()),
+    path("loans/owner/", ListLoanOwner.as_view()),
 ]
