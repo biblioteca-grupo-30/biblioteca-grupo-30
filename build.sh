d9202e61c339aff9347b1483bf2f39e21d25b7ec
Olivio Freitas <ojfneto92@gmail.com>
2023-05-10 12:14:26 -0300

    feat: alter build archive

--- build.sh
+++ build.sh
@@ -5,16 +5,16 @@
 
 
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+pip install -r requirements.txt --upgrade-debs
 
 
 echo "Make Migrations"
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 
 echo "Collect Static..."
-python3.9 manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input --clear
 
 
 echo "BUILD END"
