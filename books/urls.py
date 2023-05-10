0a09b59749e860e5df9aa0085a1ae391abb8b69e
Olivio Freitas <ojfneto92@gmail.com>
2023-05-10 10:43:51 -0300

    feat: create book permissions ok

--- books/urls.py
+++ books/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from exemplaries.views import ExamplaryRetriveUpdate, ExemplaryView
-from .views import BookView, BookRetrieveView, BookUpdateDestroyView
+from .views import (
+    BookView,
+    BookRetrieveView,
+    BookUpdateDestroyView,
+    ListBookView
+)
 
 
 urlpatterns = [
     path("books/", BookView.as_view()),
+    path("books/list/", ListBookView.as_view()),
     path("books/delete-update/<int:pk>/",
          BookUpdateDestroyView.as_view(), name="bookUpdateDestroyView"),
     path("books/<int:pk>/", BookRetrieveView.as_view(),
--- books/views.py
+++ books/views.py
@@ -2,13 +2,22 @@ from books.permissions import IsAuthenticatedOrReadOnly, IsUserAdmin
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.generics import (
-    ListCreateAPIView,
+    CreateAPIView,
+    ListAPIView,
     RetrieveAPIView,
     RetrieveUpdateDestroyAPIView)
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
-class BookView(ListCreateAPIView):
+class BookView(CreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsUserAdmin]
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class ListBookView(ListAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
 
